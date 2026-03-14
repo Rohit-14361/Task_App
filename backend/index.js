5f291bd0031624c42dc96482b6e4c7c587823a7a
Rohit Kumar <163702263+Rohit-14361@users.noreply.github.com>
2026-03-14 21:06:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: ['http://localhost:3000',"https://task-app-rvad.onrender.com/"],
   credentials: true
 }));
 
